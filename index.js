3c0be9c6a56d0f485b49b6125c968e35abc09938
M.S.T.O.P <mstop4@gmail.com>
2019-02-28 19:22:23 -0500

    wip

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const express = require('express');
+const request = require('request');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  const baseUrl = 'https://api.pushshift.io/reddit/submission/search';
+  const query = {
+    subreddit: 'TheSimpsons',
+    limit: 50
+  };
+
+  // request.get({
+  //   url: baseUrl,
+  //   qs: query
+  // })
+  request('http://www.google.com')
+    .on('response', (error, response, body) => {
+      if (error) {
+        console.log('Error');
+        console.log(response);
+        res.json(error);
+      }
+
+      else {
+        console.log('Success!');
+        console.log(response);
+        res.json(body);
+      }
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
